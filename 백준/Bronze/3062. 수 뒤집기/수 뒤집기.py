de2c39b7dc81f0aa9c7e735ebc9b9697c19cc137
최은진 <101858177+carrtt1@users.noreply.github.com>
2024-08-31 21:17:23 +0900

    [Bronze II] Title: 수 뒤집기, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/3062. 수 뒤집기/수 뒤집기.py
+++ 백준/Bronze/3062. 수 뒤집기/수 뒤집기.py
@@ -5,8 +5,6 @@ for test_case in range(1, T+1) :
     numr = num[::-1]
     test = int(num) + int(numr)
     sen = list(map(int, str(test)))
-    # print(sen)
-    # print(sen[::-1])
     flag = 0
 
     if sen == sen[::-1] :
