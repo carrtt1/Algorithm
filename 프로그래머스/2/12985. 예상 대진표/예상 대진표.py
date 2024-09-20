6c11ce2c5d5ae458311bfc948ce7b31461f17d2f
최은진 <101858177+carrtt1@users.noreply.github.com>
2024-09-20 15:20:32 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,14 @@
+def solution(n,a,b):
+    answer = 0
+
+    while a!= b :
+        answer += 1
+        if a%2 == 1 :
+            a = a//2+1
+        else :
+            a //= 2
+        if b%2 == 1 :
+            b = b//2+1
+        else :
+            b //= 2
+    return answer
